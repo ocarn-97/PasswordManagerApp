a7f8003fd158fa5f562a4621dbfce459a793fa93
Owen <carneyof@outlook.com>
2024-05-02 17:06:49 -0400

    Add project files.

---
+++ PasswordManagerApi/Models/AccountModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PasswordManagerApi.Models
+{
+    public class AccountModel
+    {
+        public string? Title { get; set; }
+        [Url]
+        public string? Url { get; set; }
+        [EmailAddress]
+        public string? Email { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Notes { get; set; }
+        public bool? Favorite { get; set; }
+    }
+}
