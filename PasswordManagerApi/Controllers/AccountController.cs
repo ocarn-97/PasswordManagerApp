b8cfea614639958633b61a9f1b6c281d7e40dd26
Owen <carneyof@outlook.com>
2024-05-02 21:41:05 -0400

    Added notes to AccountController

--- PasswordManagerApi/Controllers/AccountController.cs
+++ PasswordManagerApi/Controllers/AccountController.cs
@@ -1,5 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PasswordManagerApi.Data;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using System.Buffers.Text;
+using System.Collections.Generic;
+
+//-Resource based
+//- get account by account ID
+//	- GET account/:ID
+//        - return account in JSON body
+//	- POST account/new
+
+//        - return account ID
+//    - PUT account /:ID / update
+//        - return 204 or 200 with updated account info
+//	- DEL account/:ID
+//        - return 204
+//- get accounts for given userid
+//	- GET :userID / accounts
+//        - return list of json bodies
 
 namespace PasswordManagerApi.Controllers
 {
