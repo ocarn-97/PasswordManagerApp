a60d070b33e3ea8755f6ad8955fcebb10f0e0ed7
Owen <carneyof@outlook.com>
2024-05-11 16:02:33 -0400

    Finished primary requests in AccountController.

--- PasswordManagerApi/Controllers/AccountController.cs
+++ PasswordManagerApi/Controllers/AccountController.cs
@@ -11,7 +11,7 @@ using PasswordManagerApi.Models;
 //	- POST account/new
 //        - return account ID
 
-//    - PUT account /:ID / update
+//  - PUT account /:ID / update
 
 //        - return 204 or 200 with updated account info
 
@@ -24,13 +24,18 @@ using PasswordManagerApi.Models;
 
 namespace PasswordManagerApi.Controllers
 {
-    [Route("api/account")]
+    /// <summary>
+    /// Retrieves all accounts.
+    /// </summary>
+    /// <remarks>
+    /// This method retrieves all accounts stored in the system.
+    /// </remarks>
+    [Route("api/accounts")]
     [ApiController]
     public class AccountController(ApplicationDbContext applicationDbContext) : ControllerBase
     {
-
-        [HttpGet]
-        public IActionResult GetAccounts()
+        [HttpGet("all")]
+        public IActionResult GetAllAccounts()
         {
             var accounts = applicationDbContext.Accounts.ToList();
 
@@ -45,7 +50,7 @@ namespace PasswordManagerApi.Controllers
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetAccountById(int id)
+        public IActionResult GetAccountById([FromRoute] int id)
         {
             var account = applicationDbContext.Accounts.Find(id);
 
@@ -59,8 +64,8 @@ namespace PasswordManagerApi.Controllers
             }
         }
 
-        [HttpPost]
-        public IActionResult CreateAccount(AccountModel account)
+        [HttpPost("new")]
+        public IActionResult CreateAccount([FromBody] AccountModel account)
         {
             if (account == null)
             {
@@ -70,11 +75,59 @@ namespace PasswordManagerApi.Controllers
             {
                 applicationDbContext.Accounts.Add(account);
                 applicationDbContext.SaveChanges();
-                return Ok("Account created successfully");
+                return CreatedAtAction(nameof(GetAccountById), new {id = account.Id}, new { id = account.Id });
             }
             catch (Exception ex)
             {
+                return StatusCode(500, ex.Message);
+            }
+        }
 
+        [HttpPut("update/{id}")]
+        public IActionResult UpdateAccount([FromRoute] int id, [FromBody] AccountModel account)
+        {
+            var accountToUpdate = applicationDbContext.Accounts.Find(id);
+
+            if (accountToUpdate == null)
+            {
+                return NotFound();
+            }
+            try
+            {
+                accountToUpdate.Title = account.Title;
+                accountToUpdate.Url = account.Url;
+                accountToUpdate.Email = account.Email;
+                accountToUpdate.Username = account.Username;
+                accountToUpdate.Password = account.Password;
+                accountToUpdate.Notes = account.Notes;
+                accountToUpdate.Favorite = account.Favorite;
+
+                applicationDbContext.SaveChanges();
+                return Ok(accountToUpdate);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete("delete/{id}")]
+        public IActionResult DeleteAccount([FromRoute] int id)
+        {
+            var accountToDelete = applicationDbContext.Accounts.Find(id);
+
+            if (accountToDelete == null)
+            {
+                return NotFound();
+            }
+            try
+            {
+                applicationDbContext.Remove(id);
+                applicationDbContext.SaveChanges();
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
                 return StatusCode(500, ex.Message);
             }
         }
